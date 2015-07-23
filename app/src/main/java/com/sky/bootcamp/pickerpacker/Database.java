7b73cc8c5ac1d9dc5507d7fe7410e2ddcebbed39
benjy2429 <ben-carr7@hotmail.com>
2015-07-23 17:07:40 +0100

    added the pick activity with a list view of all orderlines that need picking (from an array)

--- app/src/main/java/com/sky/bootcamp/pickerpacker/Database.java
+++ app/src/main/java/com/sky/bootcamp/pickerpacker/Database.java
@@ -14,7 +14,7 @@ public class Database {
     /**
      * Connection string.
      */
-    private static String url = "jdbc:postgresql://192.168.0.14/"; // DATABASE CONNECTION STRING
+    private static String url = "jdbc:postgresql://192.168.0.13/"; // DATABASE CONNECTION STRING
     /**
      * Username for database
      */
