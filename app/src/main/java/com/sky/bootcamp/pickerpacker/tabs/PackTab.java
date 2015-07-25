66d6e2b5a5f458fed345942d15c15e9553e61f5e
benjy2429 <ben-carr7@hotmail.com>
2015-07-25 15:00:33 +0100

    added a new activity for tabs, will integrate this with the list view next

---
+++ app/src/main/java/com/sky/bootcamp/pickerpacker/tabs/PackTab.java
@@ -0,0 +1,22 @@
+package com.sky.bootcamp.pickerpacker.tabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sky.bootcamp.pickerpacker.R;
+
+/**
+ * Created by bca23 on 25/07/15.
+ */
+public class PackTab extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_pick,container,false);
+        return v;
+    }
+}
