bd59fd7069ebd4b713306fd982e73fa90ef688a8
Martin Ghosal <martin_ghosal@hotmail.co.uk>
2015-07-27 21:12:56 +0100

    Fixed the loginhelper

--- app/src/main/java/com/sky/bootcamp/pickerpacker/helpers/LoginHelper.java
+++ app/src/main/java/com/sky/bootcamp/pickerpacker/helpers/LoginHelper.java
@@ -1,14 +1,11 @@
 package com.sky.bootcamp.pickerpacker.helpers;
 
 import java.io.UnsupportedEncodingException;
-import java.text.Collator;
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import android.util.Base64;
+
 import org.spongycastle.crypto.digests.SHA256Digest;
 import org.spongycastle.crypto.generators.PKCS5S2ParametersGenerator;
 import org.spongycastle.crypto.params.KeyParameter;
+import org.spongycastle.util.encoders.Base64;
 
 /**
  * Created by bca23 on 27/07/15.
@@ -26,21 +23,19 @@ public class LoginHelper {
         String storedHash = parts[3];
         String calculatedHash = getEncodedHash(enteredPassword, salt);
 
-        // TODO FIX MEEEEE
         System.out.println(storedHash);
         System.out.println(calculatedHash);
 
-        return (Base64.encodeToString(storedHash.getBytes(), Base64.DEFAULT)).equals(calculatedHash);
+        //I just return this
+        return storedHash.equals(calculatedHash);
     }
 
     public static String getEncodedHash(String password, String salt) throws UnsupportedEncodingException {
-        // Returns only the last part of whole encoded password
         PKCS5S2ParametersGenerator gen = new PKCS5S2ParametersGenerator(new SHA256Digest());
 
-        gen.init(password.getBytes("UTF-8"), salt.getBytes(), DEFAULT_ITERATIONS);
+        gen.init(password.getBytes(), salt.getBytes(), DEFAULT_ITERATIONS);
         byte[] dk = ((KeyParameter) gen.generateDerivedParameters(256)).getKey();
 
-        return Base64.encodeToString(dk, Base64.DEFAULT);
+        return Base64.toBase64String(dk);
     }
-
 }
