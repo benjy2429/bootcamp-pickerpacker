3b46ddeef769d0970b07df3f7b9e82862e846b30
Martin Ghosal <martin_ghosal@hotmail.co.uk>
2015-07-28 10:26:24 +0100

    Changed password to passwordHash in User.java

--- app/src/main/java/com/sky/bootcamp/pickerpacker/database/DatabaseAccessLayer.java
+++ app/src/main/java/com/sky/bootcamp/pickerpacker/database/DatabaseAccessLayer.java
@@ -1,7 +1,28 @@
 package com.sky.bootcamp.pickerpacker.database;
 
+import com.sky.bootcamp.pickerpacker.models.User;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by mgh01 on 27/07/2015.
  */
 public class DatabaseAccessLayer {
+
+    private static Connection conn = Database.GetConnection();
+
+    public static User getUsers(String email){
+
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT email FROM auth_user WHERE email="+email);
+        }catch(SQLException e){
+            System.err.print(e);
+        }
+        return null;
+    }
+
 }
